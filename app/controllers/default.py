f43253dfe753d1ae012debaa3b723301ee2b785b
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2020-09-03 14:02:28 -0300

    last functions

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,7 +1,6 @@
 from app import app
 from flask import Flask, render_template, session
-from app.models.tables import Employee
-from app.controllers import default
+
 
 @app.route("/")
 def Index():
--- app/controllers/employee.py
+++ app/controllers/employee.py
@@ -1,9 +1,47 @@
-from app import app
-from flask import Flask, render_template, session
+from app import app,db
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from app.models.tables import Employee
 from app.controllers import default
 
 @app.route("/main")
 def main():
-    print(session["company_id"])
-    return render_template("index.html")
+    all_data = Employee.query.filter_by(company_id = session['company_id']).all()
+    return render_template("index.html", employees = all_data)
+
+@app.route("/addEmployee", methods = ['POST'])
+def addEmployee():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        employee = Employee(name, email, phone, session['company_id'])
+        db.session.add(employee)
+        db.session.commit()
+        return redirect(url_for('main'))
+    else:
+        return redirect(url_for('main'))
+
+@app.route("/update", methods = ['GET', 'POST'])
+def update():
+    if request.method == 'POST':
+        my_data = Employee.query.get(request.form.get('id'))
+
+        my_data.name = request.form['name']
+        my_data.email = request.form['email']
+        my_data.phone = request.form['phone']
+
+        db.session.commit()
+
+        flash("Employee updated sucessfully")
+
+        return redirect(url_for('main')) 
+
+@app.route("/delete/<id>/", methods = ['GET', 'POST'])
+def delete(id):
+    my_data = Employee.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+
+    flash("DEleted successfully")
+    return redirect(url_for('main'))
